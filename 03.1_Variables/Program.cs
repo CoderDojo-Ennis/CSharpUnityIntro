b5d066b3efd2b8d7b902010c6dc97b2672fb119f
Russ Painter <russp@geekymonkey.com>
2018-09-27 08:26:26 +0100

    Hello World and Variables 1 projects

---
+++ 03.1_Variables/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int one = 1;
+        int oneThirdInt = 1 / 3;
+        float oneThirdFloat = 1 / 3;
+        Console.WriteLine("One = " + one);
+        Console.WriteLine("Int   1 / 3 = " + oneThirdInt);
+        Console.WriteLine("Float 1 / 3 = " + oneThirdFloat);
+    }
+}
