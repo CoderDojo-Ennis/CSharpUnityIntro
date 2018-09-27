ea3a655b1ef038481a77841a08f40314e67cade0
Russ Painter <russp@geekymonkey.com>
2018-09-27 22:26:11 +0100

    Add Pong stub project

---
+++ 03.2_Pong/Assets/Ball/BallMoveScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMoveScript : MonoBehaviour
+{
+    public float Speed = 1;
+    public int X = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        X = X + (int)Speed;
+        transform.position = new Vector3(X, 0, 0);
+    }
+}
